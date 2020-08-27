0008b9fc9a21559ad3f40f4eb42624d6dab1ea8f
a-thread <aiden.threadgoode@gmail.com>
2020-08-27 02:48:17 -0400

    updating styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const Manager = require("./utils/lib/Manager");
-const Engineer = require("./utils/lib/Engineer");
-const Intern = require("./utils/lib/Intern");
-const questions = require("./utils/lib/questions");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const questions = require("./lib/questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,7 +9,7 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./utils/lib/htmlRenderer");
+const render = require("./lib/htmlRenderer");
 
 // function prompting user to answer questions in array
 const employees = [];
